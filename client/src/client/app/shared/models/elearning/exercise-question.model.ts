3fb73a394e4e7f04a5861ac1df8f1629913928fc
quang.nguyen <quang@pretagov.com>
2018-04-11 13:40:20 +1000

    Add exam question

--- client/src/client/app/shared/models/elearning/exercise-question.model.ts
+++ client/src/client/app/shared/models/elearning/exercise-question.model.ts
@@ -5,40 +5,55 @@ import { Model } from '../decorator';
 import { APIContext } from '../context';
 
 @Model('etraining.exercise_question')
-export class ExerciseQuestion extends BaseModel{
+export class ExamQuestion extends BaseModel{
 
     // Default constructor will be called by mapper
     constructor(){
         super();
-		
-		this.title = undefined;
-		this.content = undefined;
-		this.explanation = undefined;
-		this.type = undefined;
+        
+        this.title = undefined;
+        this.content = undefined;
+        this.explanation = undefined;
+        this.type = undefined;
         this.level = undefined;
         this.group_id = undefined;
         this.question_id = undefined;
-        this.unit_id = undefined;
+        this.exam_id = undefined;
         this.score = undefined;
         this.order = undefined;
+        this.sheet_id = undefined;
         this.group_id__DESC__ = undefined;
-	}
+    }
 
     question_id: number;
-    unit_id: number;
+    exam_id: number;
+    sheet_id: number;
     score: number;
     order: number;
+    level: string;
     title:string;
     content: string;
     explanation: string;
     type: string;
-    level: number;
     group_id: number;
     group_id__DESC__: string;
 
-    static listByExercise( context:APIContext, unitId: number): Observable<any[]> {
-        return ExerciseQuestion.search(context,[],"[('unit_id','=',"+unitId+")]");
+    static listBySheet( context:APIContext, sheetId: number): Observable<any[]> {
+        return ExamQuestion.search(context,[],"[('sheet_id','=',"+sheetId+")]");
     }
 
+    static countByExam( context:APIContext, examId: number): Observable<any[]> {
+        return ExamQuestion.count(context,"[('exam_id','=',"+examId+")]");
+    }
+
+    static listOpenQuestionByExam( context:APIContext, examId: number): Observable<any[]> {
+        return ExamQuestion.search(context,[],"[('exam_id','=',"+examId+"),('type','=','ext')]");
+    }
+
+    static byQuestion( context:APIContext, questionId: number): Observable<any[]> {
+        return ExamQuestion.search(context,[],"[('question_id','=',"+questionId+")]").map(questions =>{
+            return questions.length ? questions[0]: null;
+        });
+    }
 
 }
--- client/src/client/app/shared/models/elearning/question_selector.model.ts
+++ client/src/client/app/shared/models/elearning/question_selector.model.ts
@@ -14,11 +14,19 @@ export class QuestionSelector extends BaseModel{
         this.group_id = undefined;
         this.mode = undefined;
         this.include_sub_group = undefined;
+        this.sheet_id = undefined;
+        this.score = undefined;
     }
 
     mode: string;
     number: number;
     level: string;
     group_id: number;
+    score: number;
     include_sub_group: boolean;
+    sheet_id: number;
+
+    static listBySheet( context:APIContext, sheetId: number): Observable<any[]> {
+        return QuestionSelector.search(context,[],"[('sheet_id','=',"+sheetId+")]");
+    }
 }
