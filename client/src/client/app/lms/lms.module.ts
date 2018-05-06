944114f02c01578bddb536c730ee847991342103
chinhphamvanvn <chinhphamvanvn@gmail.com>
2018-05-06 11:41:45 +0700

    options exams warning

--- client/src/client/app/lms/lms.module.ts
+++ client/src/client/app/lms/lms.module.ts
@@ -25,9 +25,11 @@ import { ExamManageComponent } from './exam/exam-manage/exam-manage.component';
 import { SubmissionDialog } from './exam/submission-dialog/submission.dialog.component';
 import { CourseUnitStudyDialog } from './course/course-unit-study-dialog/course-unit-study.dialog.component';
 import {WebcamModule} from 'ngx-webcam';
+import {MessagesModule} from 'primeng/messages';
+import {MessageModule} from 'primeng/message';
 
 @NgModule({
-    imports: [ErpSharedModule, CMSModule, AssessmentModule, AuthModule, WebcamModule],
+    imports: [ErpSharedModule, CMSModule, AssessmentModule, AuthModule, WebcamModule,MessagesModule,MessageModule],
     declarations: [LMSComponent, ExamListComponent, ExamStudyDialog,AnswerPrintDialog,
     				CourseListComponent, QuestionMarkingDialog,CertificatePrintDialog,
     				 SubmissionDialog,ClassExamListDialog,ExamManageComponent,CourseUnitStudyDialog,
