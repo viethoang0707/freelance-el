1ab27a0cbf262906c2d38ac21c62a533c6e400d2
chinhphamvanvn <chinhphamvanvn@gmail.com>
2018-05-09 11:52:35 +0700

    Task 58

--- client/src/client/app/cms/course/course-unit-template/exercise/exercise-unit.component.ts
+++ client/src/client/app/cms/course/course-unit-template/exercise/exercise-unit.component.ts
@@ -15,6 +15,7 @@ import { SelectQuestionsDialog } from '../../../../shared/components/select-ques
 import { QuestionContainerDirective } from '../../../../assessment/question/question-template/question-container.directive';
 import { IQuestion } from '../../../../assessment/question/question-template/question.interface';
 import { QuestionRegister } from '../../../../assessment/question/question-template/question.decorator';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
 	moduleId: module.id,
@@ -30,6 +31,7 @@ export class ExerciseCourseUnitComponent extends BaseComponent implements ICours
 	selectedNode: TreeNode;
 	selectedQuestions: Question[];
 	questions:Question[];
+	public subscription : Subscription;
 
 	@Input() mode;
 	unit: CourseUnit;
@@ -112,15 +114,40 @@ export class ExerciseCourseUnitComponent extends BaseComponent implements ICours
 	}
 
 	selectQuestion() {
-		this.removeOldQuestions().subscribe(() => {
-			this.questionDialog.show();
-			this.questionDialog.onSelectQuestions.subscribe(questions => {
-				this.createExerciseQuestionFromQuestionBank(questions).subscribe(examQuestions => {
-					this.exerciseQuestions =  examQuestions;
-				});
+		this.questionDialog.show();
+		this.subscription = this.questionDialog.onSelectQuestions.subscribe(questions => {
+			var subscriptions = [];
+			_.each(questions, (question:Question)=> {
+				var exerciseQuestion = new ExerciseQuestion();
+				exerciseQuestion.unit_id = this.unit.id;
+				exerciseQuestion.question_id = question.id;
+				exerciseQuestion.type =  question.type;
+				exerciseQuestion.title =  question.title;
+				
+				subscriptions.push(exerciseQuestion.save(this));
+				this.subscription.unsubscribe();
+			});
+			Observable.zip(...subscriptions).subscribe(exerciseQuestions => {
+				this.loadExerciseQuestions();
 			});
 		});
 	}
+	loadExerciseQuestions() {
+		ExerciseQuestion.listByExercise(this, this.unit.id).subscribe(exerciseQuestions => {
+			this.exerciseQuestions =  exerciseQuestions;
+			});
+	}
+
+	// selectQuestion() {
+	// 	this.removeOldQuestions().subscribe(() => {
+	// 		this.questionDialog.show();
+	// 		this.questionDialog.onSelectQuestions.subscribe(questions => {
+	// 			this.createExerciseQuestionFromQuestionBank(questions).subscribe(examQuestions => {
+	// 				this.exerciseQuestions =  examQuestions;
+	// 			});
+	// 		});
+	// 	});
+	// }
 
 	createExerciseQuestionFromQuestionBank(questions: Question[]):Observable<any> {
 		if (this.unit.id) {
@@ -187,8 +214,13 @@ export class ExerciseCourseUnitComponent extends BaseComponent implements ICours
                     return question.delete(this);
                 });
                 Observable.forkJoin(...subscriptions).subscribe(()=> {
-                    this.selectedQuestions = [];
-					this.render(this.unit);
+                    // this.selectedQuestions = [];
+					if(this.exerciseQuestions.length > 1){
+						this.render(this.unit);
+					}
+					else{
+						this.exerciseQuestions = [];
+					}
                 });
             });
 	}
