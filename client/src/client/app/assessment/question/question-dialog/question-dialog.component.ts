dc8421172caa5c9b8904559fafb819853022cba7
chinhphamvanvn <chinhphamvanvn@gmail.com>
2018-05-30 16:12:33 +0700

    task 152 p3 question-dialog.component

--- client/src/client/app/assessment/question/question-dialog/question-dialog.component.ts
+++ client/src/client/app/assessment/question/question-dialog/question-dialog.component.ts
@@ -19,14 +19,15 @@ import { QuestionRegister } from '../question-template/question.decorator';
 	templateUrl: 'question-dialog.component.html',
 })
 export class QuestionDialog extends BaseDialog<Question>  {
-	
+
 	private tree: TreeNode[];
 	private selectedNode: TreeNode;
 	@ViewChild(QuestionContainerDirective) questionHost: QuestionContainerDirective;
 	private componentRef: any;
 	private treeUtils: TreeUtils;
+	private obj: any;
 
-	constructor(private componentFactoryResolver: ComponentFactoryResolver,private changeDetectionRef: ChangeDetectorRef) {
+	constructor(private componentFactoryResolver: ComponentFactoryResolver, private changeDetectionRef: ChangeDetectorRef) {
 		super();
 		this.treeUtils = new TreeUtils();
 	}
@@ -39,6 +40,7 @@ export class QuestionDialog extends BaseDialog<Question>  {
 
 	ngOnInit() {
 		this.onShow.subscribe(object => {
+			this.obj = { selectedNode: object.group_id, title: object.title, level: object.level };
 			Group.listQuestionGroup(this).subscribe(groups => {
 				this.tree = this.treeUtils.buildGroupTree(groups);
 				if (object.group_id) {
@@ -73,12 +75,19 @@ export class QuestionDialog extends BaseDialog<Question>  {
 		})
 	}
 
-	hide(){
-		this.display= false;
+	hide() {
+		this.display = false;
 	}
 
-	saveQuestion(){
+	saveQuestion() {
 		this.save();
+	}
+
+	out() {
+		this.object.title = this.obj.title;
+		this.object.level = this.obj.level;
+		this.object.group_id = this.obj.selectedNode;
+		this.display = false;
 	}
 }
 
