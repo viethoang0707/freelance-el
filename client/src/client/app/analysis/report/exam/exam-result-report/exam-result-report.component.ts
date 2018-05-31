cad21e40bbef3f00f995492391f8574e3a3c65f0
quang.nguyen <quang@pretagov.com>
2018-06-01 02:55:23 +1000

    Add stats to gradebook

--- client/src/client/app/analysis/report/exam/exam-result-report/exam-result-report.component.ts
+++ client/src/client/app/analysis/report/exam/exam-result-report/exam-result-report.component.ts
@@ -99,7 +99,7 @@ export class ExamResultReportComponent extends BaseComponent {
                 record["grade"] = grade.name;
         }
         if (logs && logs.length) {
-            var result = this.reportUtils.analyzeExamActivity(logs);
+            var result = this.reportUtils.analyzeExamMemberActivity(logs);
             if (result[0])
                 record["date_attempt"] = this.datePipe.transform(result[0], EXPORT_DATETIME_FORMAT);
         }
