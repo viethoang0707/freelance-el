17536dc70f728e0e2b767d1ed1a2cf526459a931
Keyz1022 <ntngoc1022@gmail.com>
2018-06-28 10:12:04 +0700

    merge

--- client/src/client/app/assessment/survey/survey-dialog/survey-dialog.component.ts
+++ client/src/client/app/assessment/survey/survey-dialog/survey-dialog.component.ts
@@ -88,7 +88,7 @@ export class SurveyDialog extends BaseDialog<Survey> {
         this.usersDialog.show();
         this.usersDialog.onSelectUsers.first().subscribe(users => {
             if (users.length > 1) {
-                this.error('You can select only one editor.');
+                this.error(this.translateService.instant('You can select only one editor.'));
                 return;
             } else if (users.length == 1) {
                 var user = users[0];
