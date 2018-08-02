4e0534c6fd398aeea5c22026f424378127635c9c
Keyz1022 <ntngoc1022@gmail.com>
2018-08-02 17:16:23 +0700

    edit page login

--- frontend/src/client/app/auth/login/login.component.ts
+++ frontend/src/client/app/auth/login/login.component.ts
@@ -11,7 +11,8 @@ import { User } from '../../shared/models/elearning/user.model';
 @Component({
     moduleId: module.id,
     selector: 'login',
-    templateUrl: 'login.component.html'
+    templateUrl: 'login.component.html',
+    styleUrls: ['login.component.css']
 })
 
 export class LoginComponent extends BaseComponent implements OnInit {
