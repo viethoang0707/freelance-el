ad7956dc4c6569c307327fc4e45d8a2a5716867c
quang.nguyen <quang@pretagov.com>
2018-08-04 23:39:07 +1000

    Fix view course study

--- database/etraining/models/course.py
+++ database/etraining/models/course.py
@@ -406,13 +406,11 @@ class CourseMember(models.Model):
 	@api.model
 	def create(self, vals):
 		members = []
-		if vals:
-			raise Exception(str(vals))
 		if 'user_id' in vals and vals['user_id']:
 			if 'class_id' in vals and vals['class_id']:
-				members = self.env['etraining.course_member'].search([('user_id','=',vals['user_id']),('role','=',"'"+vals['role']+"'"),('course_id','=',vals['course_id']),('class_id','=',vals['class_id'])])
+				members = self.env['etraining.course_member'].search([('user_id','=',vals['user_id']),('role','=',vals['role']),('course_id','=',vals['course_id']),('class_id','=',vals['class_id'])])
 			else:
-				members = self.env['etraining.course_member'].search([('user_id','=',vals['user_id']),('role','=',"'"+vals['role']+"'"),('course_id','=',vals['course_id'])])
+				members = self.env['etraining.course_member'].search([('user_id','=',vals['user_id']),('role','=',vals['role']),('course_id','=',vals['course_id'])])
 		if len(members) > 0:
 			m =  members[0]
 		else:
