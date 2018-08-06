f9affedd2205f15c623e1965af4747cf98bf6aed
viethoang.070793 <viethoang.070793@gmail.com>
2018-08-06 16:57:21 +0700

    fix bug 40 translate

--- frontend/src/client/app/cms/course/course-syllabus/course-syllabus.dialog.component.ts
+++ frontend/src/client/app/cms/course/course-syllabus/course-syllabus.dialog.component.ts
@@ -116,7 +116,7 @@ export class CourseSyllabusDialog extends BaseComponent {
 		unit.parent_id = this.selectedNode ? this.selectedNode.data.id : null;
 		unit.order = maxOrder;
 		unit.save(this).subscribe(() => {
-			this.success(this.translateService.instant('Action completed');
+			this.success(this.translateService.instant('Action completed'));
 			if (this.selectedNode)
 				this.sylUtils.addChildNode(this.selectedNode, unit);
 			else
